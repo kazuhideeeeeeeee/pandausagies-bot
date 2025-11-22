ae2bdd9d2a8e2cf9f5d2d697920c53c13a67ce96
宮坂和秀 <miyasakakazuhide@kurohyounoiMac.local>
2025-11-22 13:03:32 +0900

    fix env var names

--- bot.py
+++ bot.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("pnBPfESa9gXarM9lIZW4RCDXz")
-API_SECRET = os.getenv("bRk5t7h7VGXWiRiBroGuixWO9ERA2jfgdGQCjHMX7l8zesopKM")
-ACCESS_TOKEN = os.getenv("1988900673572974592-nbefOZcMbZ1vt2MyL2q9xPiQE4wYkk")
-ACCESS_TOKEN_SECRET = os.getenv("ZtLmzo4GP5zhoxnBpaGzNZsYH9ppl4JfEz4j3Kz0Zp00t")
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+
+print("DEBUG API_KEY is None? ->", API_KEY is None)
 
 def create_client_v2():
     return tweepy.Client(
@@ -17,11 +19,12 @@ def create_client_v2():
         access_token_secret=ACCESS_TOKEN_SECRET
     )
 
+
 def main():
-        client = create_client_v2()
-        text = "テスト投稿：GitHub + Render 自動投稿ボットからの投稿です。"
-        response = client.create_tweet(text=text)
-        print("投稿完了:", response)
+    client = create_client_v2()
+    text = "テスト投稿 : GitHub + Render 自動投稿ボットからの投稿です。"
+    response = client.create_tweet(text=text)
+    print("投稿完了:", response)
 
 if __name__ == "__main__":
     main()
